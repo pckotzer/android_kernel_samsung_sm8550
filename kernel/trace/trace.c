f1bbd26a798e5e7232445bef271d0728a6f37dd4
Greg Kroah-Hartman <gregkh@google.com>
2024-08-13 17:00:25 +0000

    Revert "tracing: Use .flush() call to wake up readers"
    This reverts commit 898127d612a2ce877ca20021922bf5232ab7e688 which is
    commit e5d7c1916562f0e856eb3d6f569629fcd535fed2 upstream.
    
    It breaks the Android kernel build and can be brought back in the future
    in an abi-safe way if it is really needed.
    
    Bug: 161946584
    Change-Id: I59ae44963d713c3f116d8a6d02dfa36dda183e44
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -8189,20 +8189,6 @@ tracing_buffers_read(struct file *filp, char __user *ubuf,
 	return size;
 }
 
-static int tracing_buffers_flush(struct file *file, fl_owner_t id)
-{
-	struct ftrace_buffer_info *info = file->private_data;
-	struct trace_iterator *iter = &info->iter;
-
-	iter->wait_index++;
-	/* Make sure the waiters see the new wait_index */
-	smp_wmb();
-
-	ring_buffer_wake_waiters(iter->array_buffer->buffer, iter->cpu_file);
-
-	return 0;
-}
-
 static int tracing_buffers_release(struct inode *inode, struct file *file)
 {
 	struct ftrace_buffer_info *info = file->private_data;
@@ -8397,7 +8383,6 @@ static const struct file_operations tracing_buffers_fops = {
 	.read		= tracing_buffers_read,
 	.poll		= tracing_buffers_poll,
 	.release	= tracing_buffers_release,
-	.flush		= tracing_buffers_flush,
 	.splice_read	= tracing_buffers_splice_read,
 	.llseek		= no_llseek,
 };
