a4d47bc8aabbc4103639aac8a36252cbc9e8c97d
Greg Kroah-Hartman <gregkh@google.com>
2024-08-16 14:37:58 +0000

    Merge 5.15.155 into android15-5.15-lts
    Changes in 5.15.155
    	amdkfd: use calloc instead of kzalloc to avoid integer overflow
    	net: dsa: fix panic when DSA master device unbinds on shutdown
    	wifi: ath9k: fix LNA selection in ath_ant_try_scan()
    	VMCI: Fix memcpy() run-time warning in dg_dispatch_as_host()
    	panic: Flush kernel log buffer at the end
    	cpuidle: Avoid potential overflow in integer multiplication
    	arm64: dts: rockchip: fix rk3328 hdmi ports node
    	arm64: dts: rockchip: fix rk3399 hdmi ports node
    	ionic: set adminq irq affinity
    	pstore/zone: Add a null pointer check to the psz_kmsg_read
    	tools/power x86_energy_perf_policy: Fix file leak in get_pkg_num()
    	net: pcs: xpcs: Return EINVAL in the internal methods
    	wifi: ath11k: decrease MHI channel buffer length to 8KB
    	btrfs: handle chunk tree lookup error in btrfs_relocate_sys_chunks()
    	btrfs: export: handle invalid inode or root reference in btrfs_get_parent()
    	btrfs: send: handle path ref underflow in header iterate_inode_ref()
    	net/smc: reduce rtnl pressure in smc_pnet_create_pnetids_list()
    	Bluetooth: btintel: Fix null ptr deref in btintel_read_version
    	Input: synaptics-rmi4 - fail probing if memory allocation for "phys" fails
    	pinctrl: renesas: checker: Limit cfg reg enum checks to provided IDs
    	sysv: don't call sb_bread() with pointers_lock held
    	scsi: lpfc: Fix possible memory leak in lpfc_rcv_padisc()
    	isofs: handle CDs with bad root inode but good Joliet root directory
    	media: sta2x11: fix irq handler cast
    	ALSA: firewire-lib: handle quirk to calculate payload quadlets as data block counter
    	ext4: add a hint for block bitmap corrupt state in mb_groups
    	ext4: forbid commit inconsistent quota data when errors=remount-ro
    	drm/amd/display: Fix nanosec stat overflow
    	SUNRPC: increase size of rpc_wait_queue.qlen from unsigned short to unsigned int
    	Revert "ACPI: PM: Block ASUS B1400CEAE from suspend to idle by default"
    	libperf evlist: Avoid out-of-bounds access
    	block: prevent division by zero in blk_rq_stat_sum()
    	RDMA/cm: add timeout to cm_destroy_id wait
    	Input: allocate keycode for Display refresh rate toggle
    	platform/x86: touchscreen_dmi: Add an extra entry for a variant of the Chuwi Vi8 tablet
    	ktest: force $buildonly = 1 for 'make_warnings_file' test type
    	ring-buffer: use READ_ONCE() to read cpu_buffer->commit_page in concurrent environment
    	tools: iio: replace seekdir() in iio_generic_buffer
    	usb: typec: tcpci: add generic tcpci fallback compatible
    	usb: sl811-hcd: only defined function checkdone if QUIRK2 is defined
    	ASoC: soc-core.c: Skip dummy codec when adding platforms
    	fbdev: viafb: fix typo in hw_bitblt_1 and hw_bitblt_2
    	drivers/nvme: Add quirks for device 126f:2262
    	fbmon: prevent division by zero in fb_videomode_from_videomode()
    	netfilter: nf_tables: release batch on table validation from abort path
    	netfilter: nf_tables: release mutex after nft_gc_seq_end from abort path
    	netfilter: nf_tables: discard table flag update with pending basechain deletion
    	tty: n_gsm: require CAP_NET_ADMIN to attach N_GSM0710 ldisc
    	gcc-plugins/stackleak: Ignore .noinstr.text and .entry.text
    	gcc-plugins/stackleak: Avoid .head.text section
    	virtio: reenable config if freezing device failed
    	x86/mm/pat: fix VM_PAT handling in COW mappings
    	randomize_kstack: Improve entropy diffusion
    	platform/x86: intel-vbtn: Update tablet mode switch at end of probe
    	Bluetooth: btintel: Fixe build regression
    	VMCI: Fix possible memcpy() run-time warning in vmci_datagram_invoke_guest_handler()
    	Revert "ACPI: CPPC: Use access_width over bit_width for system memory accesses"
    	Linux 5.15.155
    
    Change-Id: Ifa8dc230966b66f084bff923024aa8260c18fe8d
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -6169,6 +6169,10 @@ static int ext4_write_dquot(struct dquot *dquot)
 	if (IS_ERR(handle))
 		return PTR_ERR(handle);
 	ret = dquot_commit(dquot);
+	if (ret < 0)
+		ext4_error_err(dquot->dq_sb, -ret,
+			       "Failed to commit dquot type %d",
+			       dquot->dq_id.type);
 	err = ext4_journal_stop(handle);
 	if (!ret)
 		ret = err;
@@ -6185,6 +6189,10 @@ static int ext4_acquire_dquot(struct dquot *dquot)
 	if (IS_ERR(handle))
 		return PTR_ERR(handle);
 	ret = dquot_acquire(dquot);
+	if (ret < 0)
+		ext4_error_err(dquot->dq_sb, -ret,
+			      "Failed to acquire dquot type %d",
+			      dquot->dq_id.type);
 	err = ext4_journal_stop(handle);
 	if (!ret)
 		ret = err;
@@ -6204,6 +6212,10 @@ static int ext4_release_dquot(struct dquot *dquot)
 		return PTR_ERR(handle);
 	}
 	ret = dquot_release(dquot);
+	if (ret < 0)
+		ext4_error_err(dquot->dq_sb, -ret,
+			       "Failed to release dquot type %d",
+			       dquot->dq_id.type);
 	err = ext4_journal_stop(handle);
 	if (!ret)
 		ret = err;
