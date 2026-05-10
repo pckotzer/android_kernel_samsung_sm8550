b375d4cee3d9cb49d1989939a144999dac9ef935
brokestar233 <3765589194@qq.com>
2026-05-10 16:25:53 +0800

    lib/math: optimize int_sqrt for Oryon
    Oryon cores have unusually fast integer division, making a CLZ/LUT
    seeded Newton-Raphson isqrt implementation preferable to the generic
    shift-and-subtract algorithm for this kernel target.
    
    Replace int_sqrt() and int_sqrt64() with a hybrid implementation that
    dispatches by input range: a LUT-assisted 16-bit path, a single-Newton
    32-bit path, and a 64-bit path using three Newton iterations followed
    by a bounded final correction step.
    
    The implementation is inspired by firelzrd/isqrt, adapted here for
    Linux kernel style and for Oryon-specific performance characteristics.
    
    link: https://github.com/firelzrd/isqrt
    Change-Id: Icd6ce558f49c274f7e950d3801ce063316b642b8
    Signed-off-by: brokestar233 <3765589194@qq.com>

--- lib/math/int_sqrt.c
+++ lib/math/int_sqrt.c
@@ -2,14 +2,140 @@
 /*
  * Copyright (C) 2013 Davidlohr Bueso <davidlohr.bueso@hp.com>
  *
- *  Based on the shift-and-subtract algorithm for computing integer
- *  square root from Guy L. Steele.
+ * Oryon-optimized hybrid integer square root.
+ *
+ * This implementation draws on ideas from firelzrd/isqrt:
+ * https://github.com/firelzrd/isqrt
+ *
+ * Thanks to Masahito Suzuki for publishing the original CLZ/LUT/Newton-based
+ * integer square root implementation that inspired this Oryon-specific kernel
+ * adaptation.
+ *
+ * Oryon-specific adaptation and Linux kernel integration by:
+ * brokestar233 <3765589194@qq.com>
+ *
+ * Small inputs are routed to table-driven helpers, while larger inputs use
+ * a CLZ/LUT-seeded Newton-Raphson path that takes advantage of Oryon's fast
+ * integer divide pipeline.
  */
 
 #include <linux/export.h>
 #include <linux/bitops.h>
 #include <linux/limits.h>
 #include <linux/math.h>
+
+static const s8 int_sqrt16_delta_lut[256] = {
+	  0,  0,  0, -1,  0, -1, -1, -1,  0, -1, -1, -1, -1, -1, -1, -1,
+	  0, -1, -1, -1, -1, -1, -1, -2, -2, -1, -1, -1, -1, -1, -1, -1,
+	  0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2, -2, -2, -2, -3, -3,
+	 -3, -3, -2, -2, -2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+	  0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -2, -2, -2,
+	 -2, -2, -2, -3, -3, -3, -3, -4, -4, -4, -4, -5, -5, -5, -5, -6,
+	 -6, -5, -5, -5, -4, -4, -4, -3, -3, -3, -3, -2, -2, -2, -2, -2,
+	 -2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+	  0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+	 -1, -1, -2, -2, -2, -2, -2, -2, -2, -3, -3, -3, -3, -3, -3, -4,
+	 -4, -4, -4, -4, -4, -5, -5, -5, -5, -6, -6, -6, -6, -6, -7, -7,
+	 -7, -7, -8, -8, -8, -8, -9, -9, -9, -9,-10,-10,-10,-11,-11,-11,
+	-11,-11,-10,-10,-10, -9, -9, -9, -8, -8, -8, -7, -7, -7, -6, -6,
+	 -6, -6, -5, -5, -5, -5, -4, -4, -4, -4, -4, -3, -3, -3, -3, -3,
+	 -3, -3, -2, -2, -2, -2, -2, -2, -2, -2, -1, -1, -1, -1, -1, -1,
+	 -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+};
+
+static const u16 int_sqrt_est_lut[128] = {
+	256,257,259,261,263,265,267,269,271,273,275,277,278,280,282,284,
+	286,288,289,291,293,295,296,298,300,301,303,305,306,308,310,311,
+	313,315,316,318,320,321,323,324,326,327,329,331,332,334,335,337,
+	338,340,341,343,344,346,347,349,350,352,353,354,356,357,359,360,
+	362,364,367,370,373,375,378,381,384,386,389,391,394,397,399,402,
+	404,407,409,412,414,417,419,422,424,426,429,431,434,436,438,441,
+	443,445,448,450,452,454,457,459,461,463,465,468,470,472,474,476,
+	478,481,483,485,487,489,491,493,495,497,499,501,503,505,507,509,
+};
+
+static __always_inline u32 int_sqrt_oryon16(u16 v)
+{
+	u32 e, zone, msb, sub_bits, sub_m, h, y;
+
+	if (v <= 1)
+		return v;
+
+	e = fls(v) - 1;
+	zone = e >> 1;
+	if (!zone)
+		return 1;
+
+	msb = e & 1;
+	sub_bits = zone - 1;
+	sub_m = (v >> (e - sub_bits)) & ((1U << sub_bits) - 1);
+	h = (1U << zone) | (msb << sub_bits) | sub_m;
+	y = h + (s32)int_sqrt16_delta_lut[h];
+
+	if ((y + 1) * (y + 1) <= v)
+		y++;
+	if (y * y > v)
+		y--;
+
+	return y;
+}
+
+static __always_inline u32 int_sqrt_oryon32(u32 v)
+{
+	u32 e, half_e, frac6, y;
+
+	if (v <= 1)
+		return v;
+
+	e = fls(v) - 1;
+	half_e = e >> 1;
+	frac6 = e >= 6 ? (v >> (e - 6)) & 0x3f : (v << (6 - e)) & 0x3f;
+	y = ((u32)int_sqrt_est_lut[((e & 1) << 6) | frac6] << half_e) >> 8;
+	y |= !y;
+
+	y = (y + v / y) >> 1;
+
+	while ((u64)y * y > v)
+		y--;
+	while ((u64)(y + 1) * (y + 1) <= v)
+		y++;
+
+	return y;
+}
+
+static __always_inline u32 int_sqrt_oryon64(u64 v)
+{
+	u32 e, half_e, frac6;
+	u64 y, yy;
+
+	if (v <= 1)
+		return v;
+
+	e = fls64(v) - 1;
+	half_e = e >> 1;
+	frac6 = e >= 6 ? (u32)((v >> (e - 6)) & 0x3f) : (u32)((v << (6 - e)) & 0x3f);
+	y = ((u64)int_sqrt_est_lut[((e & 1) << 6) | frac6] << half_e) >> 8;
+	y |= !y;
+
+	y = (y + v / y) >> 1;
+	y = (y + v / y) >> 1;
+	y = (y + v / y) >> 1;
+
+	if (y > U32_MAX)
+		y = U32_MAX;
+
+	yy = y * y;
+	if (yy > v) {
+		y--;
+	} else if (y < U32_MAX) {
+		u64 yp1 = y + 1;
+
+		if (yp1 * yp1 <= v)
+			y = yp1;
+	}
+
+	return y;
+}
 
 /**
  * int_sqrt - computes the integer square root
@@ -19,32 +145,13 @@
  */
 inline unsigned long int_sqrt(unsigned long x)
 {
-	register unsigned long tmp;
-	register unsigned long place;
-	register unsigned long root = 0;
+	if (x <= U16_MAX)
+		return int_sqrt_oryon16(x);
 
-	if (x <= 1)
-		return x;
+	if (x <= U32_MAX)
+		return int_sqrt_oryon32(x);
 
-	place = 1UL << (BITS_PER_LONG - 2);
-
-	do{
-		place >>= 2;
-	}while(place > x);
-
-	do {
-		tmp = root + place;
-		root >>= 1;
-
-		if (x >= tmp)
-		{
-			x -= tmp;
-			root += place;
-		}
-		place >>= 2;
-	}while (place != 0);
-
-	return root;
+	return int_sqrt_oryon64(x);
 }
 EXPORT_SYMBOL(int_sqrt);
 
@@ -56,24 +163,13 @@ EXPORT_SYMBOL(int_sqrt);
  */
 u32 int_sqrt64(u64 x)
 {
-	u64 b, m, y = 0;
+	if (x <= U16_MAX)
+		return int_sqrt_oryon16(x);
 
-	if (x <= ULONG_MAX)
-		return int_sqrt((unsigned long) x);
+	if (x <= U32_MAX)
+		return int_sqrt_oryon32(x);
 
-	m = 1ULL << ((fls64(x) - 1) & ~1ULL);
-	while (m != 0) {
-		b = y + m;
-		y >>= 1;
-
-		if (x >= b) {
-			x -= b;
-			y += m;
-		}
-		m >>= 2;
-	}
-
-	return y;
+	return int_sqrt_oryon64(x);
 }
 EXPORT_SYMBOL(int_sqrt64);
 #endif
